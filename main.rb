5364d952de1c17fa7bb1ea38ff564ccb549c668c
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-01-27 01:24:52 +0400

    ADD class

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
-## Here we can put the class
+require './my_enumerable'
+
+class MyList
+  def initialize(*item)
+    @list = item
+  end
+  include MyEnumerable
+end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
