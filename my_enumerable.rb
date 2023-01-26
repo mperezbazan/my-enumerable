7a9c51e9acc49c067516fc0559608b68a2fdc66f
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-01-26 16:18:56 -0300

    Add my enumerable module

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,1 +1,19 @@
-#Here we can put the module
+module MyEnumerable
+  def any?
+    output = []
+    @list.each { |n| output.push(n) if yield n }
+    !output.empty?
+  end
+
+  def all?
+    output = []
+    @list.each { |n| output.push(n) if yield n }
+    output.length == @list.length
+  end
+
+  def filter
+    output = []
+    @list.each { |n| output.push(n) if yield n }
+    output
+  end
+end
